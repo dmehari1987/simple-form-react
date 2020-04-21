7424f77f0b7e2d9e3887b79681dc69ece3aa0385
Dani <meharidaniel1111@gmail.com>
2020-04-21 15:06:48 +0200

    my first form in react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class NameForm extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+            value: '',
+            selected: 'lime'
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value})
+    }
+
+    handleSubmit(e) {
+        alert("Submitted: " + this.state.value)
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit= {this.handleSubmit}>
+                <lable>
+                    Name:
+                </lable>
+                <input type= "text" value= {this.state.value} onChange= {this.handleChange} />
+                <button type= "text" value= "submit"  >
+                    Submit
+                </button>
+                <select >
+                    <option value="grapefruit" >Grapefruit</option>
+                    <option value="lime">Lime</option>
+                    <option value="coconut">Coconut</option>
+                    <option value="mango" selected= {this.state.selected}>Mango</option>
+                </select>
+                <input type= "file"/> 
+            </form>
+        )
+    }
+}
+
+ReactDOM.render(<NameForm />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
